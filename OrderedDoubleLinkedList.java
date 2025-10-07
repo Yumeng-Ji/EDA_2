b04335268bdc29d22ff4c4048c41ec36b50aedeb
Yumeng Ji <yumeng.ji103585@gmail.com>
2025-10-07 21:55:10 +0200

    Add files via upload

---
+++ OrderedDoubleLinkedList.java
@@ -0,0 +1,14 @@
+public class OrderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements OrderedListADT<T> {
+	
+	public void add(T elem){
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+
+	}
+	
+	public OrderedDoubleLinkedList<T> intersection(OrderedDoubleLinkedList<T> lista){
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+
+
+	}
+
+}
