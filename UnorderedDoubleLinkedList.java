d573d41900e93382599ace96346e1e76441922e8
Samira034 <samira.tamang247@gmail.com>
2025-10-11 10:45:08 +0200

    Update UnorderedDoubleLinkedList.java

--- UnorderedDoubleLinkedList.java
+++ UnorderedDoubleLinkedList.java
@@ -1,14 +1,41 @@
 public class UnorderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements UnorderedListADT<T> {
 	
-	public void addToFront(T elem) {
+public void addToFront(T elem){
 	// a�ade un elemento al comienzo
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+        Node<T> nuevo = new Node<>(elem);
+        if (isEmpty()) {
+            last = nuevo;
+            last.next =last;
+            last.prev =last;
+        }
+        else {
+            nuevo.next = last.next;
+            nuevo.prev =last;
+            last.next.prev = nuevo;
+            last.next = nuevo;
+        }
+        count ++;
 
 	}
 
 	public void addToRear(T elem) {
 	// a�ade un elemento al final 
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+        Node<T> nuevo = new Node<>(elem);
+        if (isEmpty()) {
+            last = nuevo;
+            last.next =last;
+            last.prev =last;
+        }
+        else {
+            nuevo.next = last.next;
+            nuevo.prev=last;
+            last.next.prev = nuevo;
+            last.next = nuevo;
+            last=nuevo;
+        }
+        count++;
 
 	}
 	
@@ -18,3 +45,4 @@ public class UnorderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements
 	}
 
 }
+
