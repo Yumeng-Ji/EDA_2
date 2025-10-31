5840947a083a209c164e2a7ff3f0af579a42e96a
Samira034 <samira.tamang247@gmail.com>
2025-10-31 14:05:59 +0100

    Enhance PruebaOrderedDoubleLinkedList with intersection tests
    Added new test cases for intersection method and added duplicate entries in the integer list.

--- PruebaOrderedDoubleLinkedList.java
+++ PruebaOrderedDoubleLinkedList.java
@@ -10,6 +10,8 @@ public class PruebaOrderedDoubleLinkedList {
 			l.add(9);
 			l.add(0);
 			l.add(20);
+            l.add(4);
+            l.add(3);
 			l.remove(new Integer(7));
 
 			
@@ -46,10 +48,46 @@ public class PruebaOrderedDoubleLinkedList {
 			System.out.println("Prueba Find ...............");
 			System.out.println("2222? " + l2.find(new Persona("", "2222")));
 			System.out.println("5555? " + l2.find(new Persona("", "5555")));
-			System.out.println("7777? " + l2.find(new Persona("", "7777")));	
-			System.out.println("8888? " + l2.find(new Persona("", "8888")));	
-			
-			
+			System.out.println("7777? " + l2.find(new Persona("", "7777")));
+			System.out.println("8888? " + l2.find(new Persona("", "8888")));
+
+            OrderedDoubleLinkedList<Persona> l3 = new OrderedDoubleLinkedList<Persona>();
+            l3.add(new Persona("olaf", "1111"));
+            l3.add(new Persona("ana", "7723"));
+            l3.add(new Persona("pedro", "2222"));
+            l3.add(new Persona("unai", "8888"));
+            l3.add(new Persona("pablo", "2232"));
+            l3.add(new Persona("olatz", "5555"));
+            System.out.print(" Lista ...............");
+            l3.visualizarNodos();
+            System.out.println(" Num elementos: " + l3.size());
+
+            System.out.println("Intersection...............");
+            l2.intersection(l3).visualizarNodos();
+
+
+
+            OrderedDoubleLinkedList<Persona> l4 = new OrderedDoubleLinkedList<Persona>();
+            System.out.println("Intersection...............");
+            l3.intersection(l4).visualizarNodos();
+
+            OrderedDoubleLinkedList<Persona> l5 = new OrderedDoubleLinkedList<Persona>();
+            System.out.println("Intersection...............");
+            l5.intersection(l4).visualizarNodos();
+
+            OrderedDoubleLinkedList<Persona> l6 = new OrderedDoubleLinkedList<Persona>();
+            l6.add(new Persona("olatz","5555"));
+            System.out.println("Intersection...............");
+            l3.intersection(l6).visualizarNodos();
+
+
+            OrderedDoubleLinkedList<Persona> l7 = new OrderedDoubleLinkedList<Persona>();
+            l7.add(new Persona("pedro","2222"));
+            System.out.println("Intersection...............");
+            l3.intersection(l7).visualizarNodos();
+            System.out.println(" Num elementos: " + l3.intersection(l7).size());
+
+        }
 	}
-	}
+
 
